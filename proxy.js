a10c0922106bd30f235ce90405f25a74ee5d2bf4
kimberly kung <kimberly.kung@gmail.com>
2021-05-25 09:00:14 -0700

    BMR

--- proxy.js
+++ proxy.js
@@ -1,20 +1,9 @@
 const express = require('express');
-const servicePorts = require('./config.js')
 
 const proxy = express();
-const port = 8000;
+const port = 3000;
 
 proxy.listen(port, () => {
   console.log(`Proxy listening at http://localhost:${port}`);
 });
-proxy.use(express.static('/public'))
-
-proxy.get('/', (req, res) => {
-    console.log('proxy request query: ', req.query);
-    let campId = parseInt(req.query.campId);
-    if (typeof campId !== 'number' || campId === undefined) {
-      campId = 0;
-    }
-    console.log('Camp ID: ', campId);
-
-})
+proxy.use(express.static('public'));
