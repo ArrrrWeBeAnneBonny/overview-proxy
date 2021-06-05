36a78cfe68696b64e41758350aff293c6b71c999
kimberly <kimberly.kung@gmail.com>
2021-06-05 13:35:46 -0700

    minor updates for production and dev modes

--- config.js
+++ config.js
@@ -1,6 +1,25 @@
 module.exports = {
-  reviews: 3001,
-  booking: 3002,
-  overview: 3003,
-  photogallery:3004
+  dev: {
+    overview: 'http://localhost:3003/',
+    reviews: 'http://localhost:3001/',
+    booking: 'http://localhost:3002/',
+    photogallery: 'http://localhost:3004/',
+    proxy: 'http://localhost:3000/'
+  },
+  production: {
+    overview: 'https://fec-overview.s3-us-west-2.amazonaws.com/overview.js',
+    reviews: 'https://reviews-avatars.s3-us-west-1.amazonaws.com/reviews.js',
+    booking: 'http://localhost:3002/',
+    photogallery: 'http://localhost:3004/',
+    proxy: 'http://ec2-52-89-7-45.us-west-2.compute.amazonaws.com/'
+  },
+  allowedOrigins: [
+    'http://localhost:3000/',
+    'http://localhost:5500/',
+    'https://fec-overview.s3-us-west-2.amazonaws.com/',
+    'http://ec2-35-163-3-32.us-west-2.compute.amazonaws.com/',
+    'http://35.163.3.32:3000/',
+    'http://35.163.3.32:80/',
+    'http://35.163.3.32/'
+  ]
 }
--- proxy.js
+++ proxy.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const config = require('./config.js');
 
 const proxy = express();
 const port = 3000;
 
+let configURL = {};
+if (process.env.NODE_ENV === "production") {
+  configURL = config.production;
+} else {
+  configURL = config.dev;
+}
+
 proxy.listen(port, () => {
-  console.log(`Proxy listening at http://localhost:${port}`);
+  console.log(`Proxy listening at ${configURL.proxy}`)
 });
 proxy.use(express.static('public'));
