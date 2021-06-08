3b9e9662b06d3de59df18162a71bef043d5b9110
kimberly <kimberly.kung@gmail.com>
2021-06-08 15:27:32 -0700

    Covid banner done

--- proxy.js
+++ proxy.js
@@ -8,12 +8,13 @@ const mode = process.env.NODE_ENV;
 let configURL = {};
 if (mode === "development") {
   configURL = config.dev;
-  proxy.use(express.static('public_dev'));
+  // proxy.use(express.static('public_dev'));
   port = 3000;
 } else {
   configURL = config.production;
-  proxy.use(express.static('public_prod'));
+  // proxy.use(express.static('public_prod'));
 }
+proxy.use(express.static('dist'));
 
 proxy.listen(port, () => {
   console.log(`Proxy listening at ${configURL.proxy}`)
