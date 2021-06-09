2c577d1009f76a62f00cfd01c7f3ff34abebff82
kimberly kung <kimberly.kung@gmail.com>
2021-06-08 20:07:43 -0700

    EOD 6/8 workign on search bar of navigation

--- client/Components/navbar.jsx
+++ client/Components/navbar.jsx
@@ -5,12 +5,26 @@ class NavBar extends React.Component {
 
   render() {
     return (
+      <div className='banners'>
       <div className='top-banner'>
-        <div class="covid-banner">
+        <div className="covid-banner">
           <div className="banner-content">
             Recreate responsibly during COVID-19. Learn more <a target="_blank" className="underlined" href="https://www.hipcamp.com/journal/search/covid"> here</a>.
           </div>
         </div>
+      </div>
+      <nav className='main-navigation-bar'>
+        <div className='left'>
+          <div className="logo"><a href="https://www.hipcamp.com/en-US"><div className="logo-placeholder">&nbsp;</div></a></div>
+          <div className="nav-bar-search">
+              <input type="search" value="" className="form-control location-search-input__input-field tt-input" placeholder="Try Yosemite, Napa, Moab..." dir="auto" />
+          </div>
+        </div>
+
+        <div className='right'>
+
+        </div>
+      </nav>
       </div>
     )
   }
